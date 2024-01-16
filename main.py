6f3b8233801ff9a9476dbcb715ffc37c2a25c2f4
shrutik bole <boleshrutik51@gmail.com>
2024-01-16 16:40:17 +0530

    validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Winequality import logger
 from Winequality.pipeline.stage_01_data_ingstion import DataIngestionTrainingPipeline
+from Winequality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 
@@ -8,6 +9,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = DataValidationTrainingPipeline()
+   obj.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/Winequality/config/configuration.py
+++ src/Winequality/config/configuration.py
@@ -1,5 +1,5 @@
 from Winequality.constants import *
-from Winequality.entity.config_entity import DataIngestionConfig
+from Winequality.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from Winequality.utils.common import read_yaml, create_directories
 
 class ConfigurationManager:
@@ -29,3 +29,19 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
